ae2410f5848a03d9eece4f9ff677cfed861f7d79
isabeldiana <isabel_diana@outlook.com>
2023-12-03 19:34:55 -0300

    Atualizacao do env

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
+require("dotenv").config();
 module.exports = {
   pool: {
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "25262829",
-    database: "dindin",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
   },
   jwt: {
-    senhaJwt: "senha_secreta",
+    senhaJwt: process.env.PASS_JWT,
     options: {
       expiresIn: "1d",
     },
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.json());
 app.use(rotas);
 
-app.listen(3000);
+app.listen(process.env.PORT);
